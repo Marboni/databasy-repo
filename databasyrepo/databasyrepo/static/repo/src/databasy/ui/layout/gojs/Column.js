1d3e0f378bf9874bc6ce4583b6c16206a1f13de0
Boris Marchenko <boma@ciklum.com>
2013-10-09 20:13:20 +0300

    [#58421230] Column icon customization.

--- databasyrepo/databasyrepo/static/repo/src/databasy/ui/layout/gojs/Column.js
+++ databasyrepo/databasyrepo/static/repo/src/databasy/ui/layout/gojs/Column.js
@@ -13,7 +13,7 @@ databasy.ui.layout.gojs.Column = Class.extendFrom(go.Panel, {
         this.margin = new go.Margin(1, 0, 1, 0);
         this.padding = new go.Margin(0, 0, 0, 6);
 
-        var tableIcon = mk(go.Shape, {
+        this.tableIcon = mk(go.Shape, {
             figure:'circle',
             width:6,
             height:6,
@@ -21,7 +21,7 @@ databasy.ui.layout.gojs.Column = Class.extendFrom(go.Panel, {
             fill:'#ffffff',
             stroke:'#007eb1'
         });
-        this.add(tableIcon);
+        this.add(this.tableIcon);
 
         this.nameField = mk(go.TextBlock, {
             position:new go.Point(22, -1),
@@ -33,9 +33,28 @@ databasy.ui.layout.gojs.Column = Class.extendFrom(go.Panel, {
     load: function() {
         var column = databasy.gw.model.node(this.columnId);
         this.setName(column.val('name'));
+        if (column.val('pk')) {
+            this.setPk();
+        } else {
+            this.setNullable(column.val('null'));
+        }
     },
 
     setName: function(name) {
         this.nameField.text = name;
+    },
+
+    setPk: function() {
+        this.tableIcon.figure = 'pointer';
+        this.tableIcon.fill = this.tableIcon.stroke;
+    },
+
+    setNullable: function(nullable) {
+        this.tableIcon.figure = 'circle';
+        if (nullable) {
+            this.tableIcon.fill = '#ffffff';
+        } else {
+            this.tableIcon.fill = this.tableIcon.stroke;
+        }
     }
 });
